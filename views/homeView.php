98468eb52560b2819daaefb504f8454e80a6c31d
valros76 <valerian.dufrene@gmail.com>
2018-07-28 10:03:39 +0200

    Ajout d'une base HTML dans le homeView.php

--- views/homeView.php
+++ views/homeView.php
@@ -1,13 +1,22 @@
-<?php
-    $title="Home - Bootstrap Training";
-    $locateCss="css/style.css";
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
+    <title>Home - Bootstrap training</title>
+</head>
+<body>
+    <div id="container">
+        <header id="head">
+            <h1>Home</h1>
+        </header>
 
-<?php ob_start();
-    echo '<h1>Home</h1>';
-$header=ob_get_clean();?>
-
-<?php ob_start();
-    echo '<p>Content...</p>';
-$content=ob_get_clean();?>
-<?php require('templates/template.php'); ?>
+        <section id="content">
+            <p>Content...</p>
+        </section>
+    </div>
+</body>
+</html>
