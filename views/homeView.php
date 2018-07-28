cae33f4b523d07bcd7de03742bfd811a4a7414db
valros76 <valerian.dufrene@gmail.com>
2018-07-28 20:16:21 +0200

    Mise à jour du index.php / Ajout de contenu

--- views/homeView.php
+++ views/homeView.php
@@ -9,44 +9,55 @@
     <title>Home - Bootstrap training</title>
 </head>
 <body>
-    <div class="container fullContent">
-        <header id="head" class="row">
-            <h1 class="col-lg-12">Home</h1>
-            <p class="col-lg-12">Style blog</p>
-        </header>
-        <div class="row">
-            <nav class="col-lg-12">
-                <fieldset class="container">
-                <legend>Menu</legend>
-                    <ul>
-                        <li><a href="">Acceuil</a></li>
-                    </ul>
-                </fieldset>
-            </nav>
-
-            <section class="row">
-                <div class="col-lg-2"><img src="img/t1.jpg" alt="Tigre"></div>
-                <div class="col-lg-2"><img src="img/t2.jpg" alt="Tigre"></div>
-                <div class="col-lg-2"><img src="img/t3.jpg" alt="Tigre"></div>
-                <div class="col-lg-2"><img src="img/t4.jpg" alt="Tigre"></div>
-                <div class="col-lg-2"><img src="img/t5.jpg" alt="Tigre"></div>
-                <div class="col-lg-2"><img src="img/t6.jpg" alt="Tigre"></div>
-                <div class="col-lg-2"><img src="img/t7.jpg" alt="Tigre"></div>
-                <div class="col-lg-2"><img src="img/t8.jpg" alt="Tigre"></div>
-                <div class="col-lg-2"><img src="img/t9.jpg" alt="Tigre"></div>
-                <div class="col-lg-2"><img src="img/t10.jpg" alt="Tigre"></div>
-                <div class="col-lg-2"><img src="img/t11.jpg" alt="Tigre"></div>
-                <div class="col-lg-2"><img src="img/t12.jpg" alt="Tigre"></div>
-            </section>
-            
+    <div class="container">
+      <header class="page-header">
+        <h1>Mon amour pour les tigres</h1>
+      </header>
+      <section class="row">
+        <div class="col-lg-12">
+          <p>
+            Je suis passionné par les <strong>tigres</strong> depuis très longtemps. Ce site a été construit en <em>hommage à ces merveilleux félins...</em><br>
+            Je fais partie de la <abbr title="Société des Amoureux des Tigres">SAT</abbr> qui a pour but de faire connaître ces splendides animaux. 
+          </p>
+          <p>Voici ce qu'en dit le wikipedia :</p>
+          <blockquote>
+            Le Tigre (Panthera tigris) est un mammifère carnivore de la famille des félidés (Felidae) du genre Panthera. 
+Aisément reconnaissable à sa fourrure rousse rayée de noir, il est le plus grand félin sauvage et l'un des plus grands carnivores du monde. 
+L'espèce est divisée en neuf sous-espèces possédant des différences mineures en termes de taille ou de comportement. Superprédateur, 
+il chasse principalement les cerfs et les sangliers, bien qu'il puisse s'attaquer à des proies de taille plus importante comme les buffles. 
+Jusqu'au XIXe siècle, le Tigre était réputé mangeur d'homme. La structure sociale des tigres en fait un animal solitaire ; le mâle possède 
+un territoire qui englobe les domaines de plusieurs femelles et ne participe pas à l'éducation des petits.<br>
+            <small class="pull-right">Wikipedia</small><br>
+          </blockquote>
         </div>
-
-        <div class="row">
-            <footer class="col-lg-12">
-                <hr/>
-                <p>Je suis un pied de page...</p>
-            </footer>
+      </section>
+      <section class="row">
+        <div class="col-xs-4 col-sm-3 col-md-2"><img src="img/t1.jpg" alt="Tigre"></div>
+        <div class="col-xs-4 col-sm-3 col-md-2"><img src="img/t2.jpg" alt="Tigre"></div>
+        <div class="col-xs-4 col-sm-3 col-md-2"><img src="img/t3.jpg" alt="Tigre"></div>
+        <div class="col-xs-4 col-sm-3 col-md-2"><img src="img/t4.jpg" alt="Tigre"></div>
+        <div class="col-xs-4 col-sm-3 col-md-2"><img src="img/t5.jpg" alt="Tigre"></div>
+        <div class="col-xs-4 col-sm-3 col-md-2"><img src="img/t6.jpg" alt="Tigre"></div>
+        <div class="col-xs-4 col-sm-3 col-md-2"><img src="img/t7.jpg" alt="Tigre"></div>
+        <div class="col-xs-4 col-sm-3 col-md-2"><img src="img/t8.jpg" alt="Tigre"></div>
+        <div class="col-xs-4 col-sm-3 col-md-2"><img src="img/t9.jpg" alt="Tigre"></div>
+        <div class="col-xs-4 col-sm-3 col-md-2"><img src="img/t10.jpg" alt="Tigre"></div>
+        <div class="col-xs-4 col-sm-3 col-md-2"><img src="img/t11.jpg" alt="Tigre"></div>
+        <div class="col-xs-4 col-sm-3 col-md-2"><img src="img/t12.jpg" alt="Tigre"></div>
+      </section>
+      <section class="row">
+        <aside class="col-sm-4">
+          <address>
+            <p>Vous pouvez me contacter à cette adresse :</p>
+            <strong>Tigrou Alfred</strong><br>
+            Allée des fauves<br>
+            28645 Félins-sur-Loire<br>
+          </address>
+        </aside>
+        <div class="col-sm-8">
+          <img src="img/photo-tigre.jpg" alt="Tigre">
         </div>
+      </section>
     </div>
-</body>
+  </body>
 </html>
