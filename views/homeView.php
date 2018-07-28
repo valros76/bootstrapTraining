c8f546535e35f4399b93a244b862ac21317dd88b
valros76 <valerian.dufrene@gmail.com>
2018-07-28 20:41:48 +0200

    Ajout de contenu sur la la view

--- views/homeView.php
+++ views/homeView.php
@@ -45,19 +45,74 @@ un territoire qui englobe les domaines de plusieurs femelles et ne participe pas
         <div class="col-xs-4 col-sm-3 col-md-2"><img src="img/t11.jpg" alt="Tigre"></div>
         <div class="col-xs-4 col-sm-3 col-md-2"><img src="img/t12.jpg" alt="Tigre"></div>
       </section>
-      <section class="row">
-        <aside class="col-sm-4">
+      <div class="row">
+        <section class="col-sm-4">
+          <h5>Voici les sous-espèces des tigres :</h5>
+          <br>
+          <ul>
+            <li>Tigre de Sibérie</li>
+            <li>Tigre de Chine méridionale</li>
+            <li>Tigre de Bali</li>
+            <li>Tigre de d'Indochine</li>
+            <li>Tigre de Malaisie</li>
+            <li>Tigre de Java</li>
+            <li>Tigre de Sumatra</li>
+            <li>Tigre du Bengale</li>
+            <li>Tigre de la Caspienne</li>
+          </ul>
+        </section>
+        <section class="col-sm-8">
+          <img src="img/photo-tigre.jpg" alt="Tigre">
+        </section>
+      </div>
+      <div class="row">
+        <section class="col-sm-8 table-responsive">
+          <table class="table table-bordered table-striped table-condensed">
+            <h4>Les menaces pour les tigres</h4>
+            <thead>
+              <tr>
+                <th>Lieu</th>
+                <th>Menace</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Grand Mekong</td>
+                <td>Demande croissante de certaines parties de l’animal pour la médecine chinoise traditionnelle 
+et fragmentation des habitats du fait du développement non durable d’infrastructures</td>
+              </tr>
+              <tr>
+                <td>Île de Sumatra</td>
+                <td>Production d’huile de palme et de pâtes à papiers</td>
+              </tr>
+              <tr>
+                <td>Indonésie et Malaisie</td>
+                <td>Pâte à papier, l’huile de palme et le caoutchouc</td>
+              </tr>
+              <tr>
+                <td>États-Unis</td>
+                <td>Les tigres captifs représentent un danger pour les tigres sauvages</td>
+              </tr>
+              <tr>
+                <td>Europe</td>
+                <td>Gros appétit pour l’huile de palme</td>
+              </tr>
+              <tr>
+                <td>Népal</td>
+                <td>Commerce illégal de produits dérivés de tigres</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+        <section class="col-sm-4">
           <address>
-            <p>Vous pouvez me contacter à cette adresse :</p>
-            <strong>Tigrou Alfred</strong><br>
+          <p>Vous pouvez me contacter à cette adresse :</p>
+          <strong>Tigrou Alfred</strong><br>
             Allée des fauves<br>
             28645 Félins-sur-Loire<br>
           </address>
-        </aside>
-        <div class="col-sm-8">
-          <img src="img/photo-tigre.jpg" alt="Tigre">
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   </body>
 </html>
